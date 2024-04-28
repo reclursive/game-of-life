086e88e63730edd39aeddabd4c34434dcb1216be
reclursive <imafraidofferrets@gmail.com>
2024-04-28 19:13:33 -0400

    opted out of svgs, implemented a bootstrap checkbox, changed font. Logging of board and set in motion logic started

--- src/Components/Board/firstgen.jsx
+++ src/Components/Board/firstgen.jsx
@@ -1,60 +1,277 @@
 import React from 'react'
 import './boardStyles.css'
 import wb from '../Assets/wb.svg'
-import inver from './invert'
+// import bbtiny2 from '../Assets/bbtiny2.svg'
 
 const Board = () => {
+
     return (
 <div class="container1">
     <div class="container2" className="firstBoard" >
         <div class="row row-cols-6 wbrow">
             {/* onClick={invert} for all  */}
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
             </div>
         <div class="row row-cols-6 wbrow">
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+        <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
             </div>
             <div class="row row-cols-6 wbrow">
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
             </div>
         <div class="row row-cols-6 wbrow">
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+        <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
             </div>
         <div class="row row-cols-6 wbrow">
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+        <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
             </div>
         <div class="row row-cols-6 wbrow">
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
-            <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+        <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
+            <div class="col wbbcol">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+            </div>
             </div>
         </div>
         <button className="reset">Reset</button>
---
+++ src/Components/Board/firstgen1.jsx
@@ -0,0 +1,83 @@
+// import React from 'react'
+// import './boardStyles.css'
+// import wb from '../Assets/wb.svg'
+// // import bbtiny2 from '../Assets/bbtiny2.svg'
+
+// const Board = () => {
+//     // state = {
+//     //     alive: false,
+//     //     src: wb
+//     // }
+    
+//     // const invert = (e) => {
+//     //     this.setState({alive: !this.state.alive})
+//     //     this.setState({src: bb})
+//     //onClick={this.invert}
+//     // }
+
+//     return (
+// <div class="container1">
+//     <div class="container2" className="firstBoard" >
+//         <div class="row row-cols-6 wbrow">
+//             {/* onClick={invert} for all  */}
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             </div>
+//         <div class="row row-cols-6 wbrow">
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             </div>
+//             <div class="row row-cols-6 wbrow">
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             </div>
+//         <div class="row row-cols-6 wbrow">
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             </div>
+//         <div class="row row-cols-6 wbrow">
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             </div>
+//         <div class="row row-cols-6 wbrow">
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             <div class="col wbbcol"><img src={wb} className="wbb" alt="alive" /></div>
+//             </div>
+//         </div>
+//         <button className="reset">Reset</button>
+//         <button className="reset">Set in motion</button>
+//         <button className="reset">||</button>
+//         <button className="reset">Increase Board</button>
+
+//     </div>
+
+    
+//     )
+    
+// }
+
+// export default Board
